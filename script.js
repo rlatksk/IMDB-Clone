23696958306c086f02a973d362c75f122d4708e5
rlatksk <justinsalim73@gmail.com>
2023-08-04 16:43:58 +0700

    Add new features that redirect movies card into IMDb website

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiBaseUrl = "https://api.themoviedb.org/3";
 const apiKey = "4a7a8b5e2b4e9343d4b5cf735982b83b";
-const imageBaseUrl = 'https://image.tmdb.org/t/p/w300'
+const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
 
 const moviesGrid = document.getElementById("movies-grid");
 const searchInput = document.getElementById("search-input");
@@ -8,39 +8,66 @@ const searchForm = document.getElementById("search-form");
 const categoryTitle = document.getElementById("category-title");
 
 async function fetchMoviesNowPlaying() {
-    const response = await fetch(`${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`);
-    const jsonResponse = await response.json();
-	const movies = jsonResponse.results;
-
-    displayMovies(movies);    
+  const response = await fetch(
+    `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`
+  );
+  const jsonResponse = await response.json();
+  const movies = await Promise.all(
+    jsonResponse.results.map(async (movie) => ({
+      id: movie.id,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      vote_average: movie.vote_average,
+      IMDbId: await getIMDbId(movie.id),
+    }))
+  );
+  displayMovies(movies);
 }
 
 async function searchMovies(query) {
-    const response = await fetch(`${apiBaseUrl}/search/movie?api_key=${apiKey}&query="${query}"`);
-    const jsonResponse = await response.json();
-	const movies = jsonResponse.results;
-
-    displayMovies(movies);    
+  const response = await fetch(
+    `${apiBaseUrl}/search/movie?api_key=${apiKey}&query="${query}"`
+  );
+  const jsonResponse = await response.json();
+  const movies = await Promise.all(
+    jsonResponse.results.map(async (movie) => ({
+      id: movie.id,
+      title: movie.title,
+      poster_path: movie.poster_path,
+      vote_average: movie.vote_average,
+      IMDbId: await getIMDbId(movie.id),
+    }))
+  );
+  displayMovies(movies);
 }
 
 function displayMovies(movies) {
-    moviesGrid.innerHTML = movies
-        .map(
-            (movie) => `<div class="movie-card">
+  moviesGrid.innerHTML = movies
+    .map(
+      (movie) => `<div class="movie-card">
+                            <a href="https://www.imdb.com/title/${movie.IMDbId}/">
 	                        <img src="${imageBaseUrl}${movie.poster_path}"/>
 	                        <p>⭐${movie.vote_average}</p>
 	                        <h1>${movie.title}</h1>
 	                    </div>`
-        )
-        .join("");
+    )
+    .join("");
 }
 
 function handleSearchFormSubmit(event) {
-    categoryTitle.innerHTML = "Search Results";
-    event.preventDefault();
-    const searchQuery = searchInput.value;
-    const movies = searchMovies(searchQuery);
-    displayMovies(movies);
+  categoryTitle.innerHTML = "Search Results";
+  event.preventDefault();
+  const searchQuery = searchInput.value;
+  const movies = searchMovies(searchQuery);
+  displayMovies(movies);
+}
+
+async function getIMDbId(movieId) {
+  const response = await fetch(
+    `${apiBaseUrl}/movie/${movieId}/external_ids?api_key=${apiKey}`
+  );
+  const jsonResponse = await response.json();
+  return jsonResponse.imdb_id;
 }
 
 searchForm.addEventListener("submit", handleSearchFormSubmit);
